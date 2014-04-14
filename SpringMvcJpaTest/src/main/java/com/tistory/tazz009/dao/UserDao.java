1e66fdc915c7e4b6b59b2c3140ffd6aed908595a
SeungHwan Choi <tazz009@hanmail.net>
2014-04-14 09:34:59 +0900

    add

--- SpringMvcJpaTest/src/main/java/com/tistory/tazz009/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.tistory.tazz009.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tistory.tazz009.model.User;
-
-public interface UserDao extends CrudRepository<User, String> {
-
-}
